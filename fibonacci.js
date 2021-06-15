f418e594b482c7912b0c9456b0fef5b65ce40bf9
vuongly2411 <lyhuong241@gmail.com>
2021-06-15 10:53:05 +0700

    uppp

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,13 @@
-let a=0, b=1;
-let fibonacci = "";
-for (let i=0; i<20; i++) {
-    let kq = a;
-    if (i==0) fibonacci += i;
-    else fibonacci += ", " + kq;
-    a = b;
-    b = kq + b;
+function run() {
+    let a=0, b=1;
+    let n = document.getElementById("fibonacci").value;
+    let fibonacci = "";
+    for (let i=0; i<n; i++) {
+        let kq = a;
+        if (i==0) fibonacci += i;
+        else fibonacci += ", " + kq;
+        a = b;
+        b = kq + b;
+    }
+    document.write(n + " số Fibonacci đầu tiên là: " +fibonacci);
 }
-document.write(fibonacci);
